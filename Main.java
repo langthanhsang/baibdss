6a635e75e8343e4bed7413d6647598283ff6d071
langthanhsang <langthanhsangls@gmail.com>
2021-12-22 15:02:43 +0700

    baufd

---
+++ Main.java
@@ -0,0 +1,59 @@
+package baitapabc;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ManagerHuman managerHuman = new ManagerHuman();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("*******menu******");
+            System.out.println("1.hiển thị tất cả: ");
+            System.out.println("2.hiển thị tất cả nam");
+            System.out.println("3.hiển tất cả nữ");
+            System.out.println("4. hiển thị tất cả người dưới 20 tuổi: ");
+            System.out.println("5.thêm 1 người: ");
+            System.out.println("6. xoá người theo tên: ");
+            System.out.println("7. sủa theo tên: ");
+            System.out.println("8.hiên thị theo tên nhập:  ");
+            System.out.println("0.exit");
+            choice = sc.nextInt();
+            switch (choice) {
+
+                case 1:
+                    System.out.println("1.hiển thị tất cả: ");
+                    managerHuman.displayllHuman();
+                    break;
+                case 2:
+                    System.out.println("2.hiển thị tất cả nam");
+                    managerHuman.displayMale();
+                    break;
+                case 3:
+                    System.out.println("3.hiển tất cả nữ");
+                    managerHuman.displayfeelMale();
+                    break;
+                case 4:
+                    System.out.println("4. hiển thị tất cả người dưới 20 tuổi: ");
+                    managerHuman.displaylessThan20();
+                    break;
+
+                case 5:
+                    System.out.println("5.thêm 1 người: ");
+                    managerHuman.addHuman();
+                    break;
+                case 6:
+                    System.out.println("6. xoá người theo tên: ");
+                    managerHuman.deleteByName();
+                case 7:
+                    System.out.println("7. sủa theo tên: ");
+                    managerHuman.displayByName();
+
+                case 8:
+                    System.out.println("0.exit");
+
+            }
+
+        }while (choice != 0) ;
+    }
+}
---
+++ ManagerHuman.java
@@ -0,0 +1,90 @@
+package baitapabc;
+
+import btCandy.Candy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ManagerHuman {
+    ArrayList<Human>humans = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    public void addHuman() {
+        System.out.println("nhập tên: ");
+        String name = scanner.nextLine();
+        System.out.println("nhập tuổi");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("nhập giới tính : ");
+        String gender = scanner.nextLine();
+        System.out.println("nhập địa chỉ: ");
+        String address = scanner.nextLine();
+        Human human = new Human(name,age,gender,address);
+        humans.add(human);
+    }
+
+    public void displayllHuman() {
+        for (Human human: humans) {
+            System.out.println(human);
+        }
+    }
+
+    public void displaylessThan20() {
+        for (int i = 0; i < humans.size(); i++) {
+            if(humans.get(i).getAge() < 20) {
+                System.out.println(humans.get(i));
+            }
+        }
+    }
+
+    public void displayMale() {
+
+        for (Human human: humans) {
+            if(human.getGender().equalsIgnoreCase("male")){
+                System.out.println(human);
+            }
+        }
+    }
+
+    public void displayfeelMale() {
+
+        for (Human human: humans) {
+            if(human.getGender().equalsIgnoreCase("fellmale")){
+                System.out.println(human);
+            }
+        }
+    }
+    public void deleteByName() {
+        String name = scanner.nextLine();
+        humans.removeIf(human -> human.getName().equals(name));
+    }
+
+    public void displayByName() {
+        String name = scanner.nextLine();
+        for (Human human:humans) {
+            if (human.getName().equals(name)){
+                System.out.println(human);
+            }
+
+        }
+    }
+
+    public void editByName(String name, Scanner scanner) {
+        Human humanedit = null;
+        for (Human human : humans) {
+            humanedit = human;
+            if (name == human.getName()) {
+                System.out.println("nhập tên mới :");
+                humanedit.setName(scanner.nextLine());
+                System.out.println("nhập tuổi mới : ");
+                humanedit.setAge(scanner.nextInt());
+                System.out.println("nhập giới tính mới");
+                humanedit.setGender(scanner.nextLine());
+                System.out.println("nhâp đỉa chỉ mới:");
+            }
+
+        }
+
+    }
+    }
+
